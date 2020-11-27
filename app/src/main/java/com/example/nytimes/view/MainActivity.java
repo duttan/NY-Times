2a9a6532d8c4e5330166d5980e63ded56c8344ba
duttan <duttan29@gmail.com>
2020-11-27 17:09:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/nytimes/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nytimes.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nytimes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
